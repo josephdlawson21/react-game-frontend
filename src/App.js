1d07cf17d44585ce97ff9c03ddde9e5d236cd629
github username <github email address>
2018-02-28 17:20:35 -0500

    added an event listener for the block

--- src/App.js
+++ src/App.js
@@ -3,19 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    left:'0px',
+    right:'0px',
+    top:'',
+    bottom:''
+  }
+
+  onLeft = () => {
+    this.setState({
+      left: `${parseInt(this.state.left) + 1}px`
+    },() => console.log(window))
+    // console.log('onLeft works')
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Block onLeft={this.onLeft} right={this.state.right} left={this.state.left}/>
       </div>
     );
   }
 }
+
+class Block extends React.Component {
+  componentDidMount(){
+    window.addEventListener('keypress', this.props.onLeft)
+  }
+  render(){
+      let style = {
+        position: 'relative',
+        width:'1em',
+        height:'1em',
+        left: this.props.left,
+        right: this.props.right,
+        backgroundColor: 'red'
+      }
+      return(
+        <div onClick={this.props.onLeft} style={style}/>
+      )
+    }
+  }
+
 
 export default App;
