dc9a4f90b716101d70500dbc027a1ad5925081ab
josephdlawson21 <josephdlawson21@gmail.com>
2018-03-01 10:59:48 -0500

    added tiles

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,19 +2,53 @@
 
 
 import React from 'react'
-import { Loop, Stage, World } from 'react-game-kit';
+import './App.css';
 
 class Game extends React.Component {
-  render () {
-    return(
-      <div>
-        <Loop>
-          <Stage style={{ background: '#3a9bdc' }}>
-            <World>
 
-            </World>
-          </Stage>
-        </Loop>
+
+  componentDidMount() {
+
+    let tiles = [
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,1,0,0,0,0],
+      [0,0,0,1,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0]
+    ]
+
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    let dirt = document.getElementById('dirt');
+    let grass = document.getElementById('grass');
+
+    for(let i = 0; i < tiles.length; i++){
+      for(let j = 0; j < tiles[i].length; j++){
+        if(tiles[i][j] === 0){
+          ctx.drawImage(dirt, 100*i, 100*j);
+        }else if(tiles[i][j] === 1){
+          ctx.drawImage(grass, 100*i, 100*j);
+        }
+      }
+    }
+
+  }
+
+
+  render () {
+
+    return(
+      <div className="App">
+        <h1 className="title">super cool game</h1>
+        <canvas id="canvas" width="600" height="600">
+
+        </canvas>
+        <div className="imgHider">
+          <img id="dirt" src={require('./dirt-tile.png')}/>
+          <img id="grass" src={require('./grass-tile.png')}/>
+        </div>
       </div>
     )
   }
