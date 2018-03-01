4c9cb155fcd7526c37edd04fa8c564ae94fb5632
josephdlawson21 <josephdlawson21@gmail.com>
2018-03-01 15:55:54 -0500

    BOB LIVESgit add .git add .git add .git add .

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,25 +4,91 @@
 import React from 'react'
 import './App.css';
 
+const KEY = {
+  LEFT:  37,
+  RIGHT: 39,
+  UP: 38,
+  A: 65,
+  D: 68,
+  W: 87,
+  SPACE: 32
+};
+
+//
+// const Hero = (map,x,y,ctx) => {
+//   let character = document.getElementById('character');
+//
+//   ctx.drawImage(character, x,y)
+// }
+
+class Hero extends React.Component {
+  constructor(map,x,y,ctx){
+    super()
+
+    this.character = document.getElementById('character');
+
+    ctx.drawImage(this.character, x,y)
+  }
+
+}
+
+
 class Game extends React.Component {
+  state = {
+    context: null,
+    heroX: 320,
+    heroY: 620
+  }
+  moveHero = (event) => {
+    if(event.key === 'd'){
+      if(this.state.heroX < 620){
+        this.setState({
+          heroX: this.state.heroX + 100
+        });
+      }
+    }else if(event.key === 'a'){
+      if(this.state.heroX >= 21){
+        this.setState({
+          heroX: this.state.heroX - 100
+        });
+      }
+    }else if(event.key === 'w'){
+      if(this.state.heroY >= 21){
+        this.setState({
+          heroY: this.state.heroY - 100
+        });
+      }
+    }else if (event.key === 's') {
+      if(this.state.heroY < 620){
+        this.setState({
+          heroY: this.state.heroY + 100
+        });
+      }
+    }
+  }
 
 
   componentDidMount() {
+    requestAnimationFrame(() => this.update());
+  }
 
-    let tiles = [
-      [0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0],
-      [0,0,1,0,0,0,0],
-      [0,0,0,1,0,0,0],
-      [0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0]
-    ]
-
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
+  update = () => {
     let dirt = document.getElementById('dirt');
     let grass = document.getElementById('grass');
+    let ctx = document.getElementById('canvas').getContext('2d')
+    let tiles = [
+      //left
+      [1,0,1,0,1,0,1],
+      [0,1,0,1,0,1,0],
+      [1,0,1,0,1,0,1],
+      [0,1,0,1,0,1,0], //bottom
+      [1,0,1,0,1,0,1],
+      [0,1,0,1,0,1,0],
+      [1,0,1,0,1,0,1]
+      //right
+    ]
+
+
 
     for(let i = 0; i < tiles.length; i++){
       for(let j = 0; j < tiles[i].length; j++){
@@ -34,6 +100,10 @@ class Game extends React.Component {
       }
     }
 
+    let bob = new Hero(tiles, this.state.heroX, this.state.heroY, ctx)
+    window.addEventListener('keypress', this.moveHero)
+
+    requestAnimationFrame(() => {this.update()})
   }
 
 
@@ -42,12 +112,13 @@ class Game extends React.Component {
     return(
       <div className="App">
         <h1 className="title">super cool game</h1>
-        <canvas id="canvas" width="600" height="600">
+        <canvas id="canvas" width="700" height="700">
 
         </canvas>
         <div className="imgHider">
           <img id="dirt" src={require('./dirt-tile.png')}/>
           <img id="grass" src={require('./grass-tile.png')}/>
+          <img id="character" src={require('./character-tile.png')}/>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import Game from './components/Game'
 import registerServiceWorker from './registerServiceWorker';
 
